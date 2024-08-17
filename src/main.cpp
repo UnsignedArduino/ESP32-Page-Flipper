7266066875feb6bc91f006539c9d9a9c4ae89056
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-08-17 00:44:48 -0400

    Page flipping woo

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 const gpio_num_t FORWARD_TOUCH_PAD = GPIO_NUM_14;
 const gpio_num_t BACKWARD_TOUCH_PAD = GPIO_NUM_27;
 
-// BleKeyboard bleKeyboard;
+BleKeyboard bleKeyboard("Page Flipper");
 SimpleLed led(LED_BUILTIN);
 SimpleTouch forwardTouch;
 SimpleTouch backwardTouch;
@@ -17,15 +17,22 @@ void setup() {
 
   forwardTouch.begin(FORWARD_TOUCH_PAD);
   backwardTouch.begin(BACKWARD_TOUCH_PAD);
-  //  bleKeyboard.begin();
+
+  bleKeyboard.begin();
 }
 
 void loop() {
-  //  if (bleKeyboard.isConnected()) {
-  //    led.off();
-  //  } else {
-  //    led.blink(200);
-  //  }
+  if (bleKeyboard.isConnected()) {
+    if (forwardTouch.justPressed()) {
+      bleKeyboard.write(KEY_RIGHT_ARROW);
+    }
+    if (backwardTouch.justPressed()) {
+      bleKeyboard.write(KEY_LEFT_ARROW);
+    }
+    led.setState(forwardTouch.isTouched() || backwardTouch.isTouched());
+  } else {
+    led.blink(200);
+  }
 
   forwardTouch.update();
   backwardTouch.update();
